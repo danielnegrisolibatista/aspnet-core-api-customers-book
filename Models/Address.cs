a34a92f26ec9030e61e84ca0d7226ab1c54623ef
Daniel Negrisoli Batista <danielnegrisoli@gmail.com>
2020-09-09 22:46:57 -0300

    Data annotations included in Models

--- Models/Address.cs
+++ Models/Address.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,21 @@ namespace aspnet_core_api_data_driven_customers_book.Models
 {
     public class Address
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [MaxLength(255, ErrorMessage = "This field should contains between 10 and 255 characteres")]
+        [MinLength(10, ErrorMessage = "This field should contains between 10 and 255 characteres")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
         public string State { get; set; }
-        public string ZipCode { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public string ZipCode { get; set; } 
     }
 }
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,20 @@ namespace aspnet_core_api_data_driven_customers_book.Models
 {
     public class Customer
     {
+        [Key]
         public int Id { get; set; }
+        
+        [Required(ErrorMessage = "This field is required")]
+        [MaxLength(255, ErrorMessage = "This field should contains between 2 and 255 characteres")]
+        [MinLength(2, ErrorMessage = "This field should contains between 2 and 255 characteres")]
         public int FirstName { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        [MaxLength(255, ErrorMessage = "This field should contains between 2 and 255 characteres")]
+        [MinLength(2, ErrorMessage = "This field should contains between 2 and 255 characteres")]
         public int LastName { get; set; }
-        public int Birthday { get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
+        public DateTime Birthday { get; set; }
     }
 }
