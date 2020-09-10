12a103266931b92a2b5e2c9ef62ed8ee18f164c5
Daniel Negrisoli Batista <danielnegrisoli@gmail.com>
2020-09-09 22:39:16 -0300

    Added the basic models of Customer and Address

---
+++ Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet_core_api_data_driven_customers_book.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet_core_api_data_driven_customers_book.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+        public int Birthday { get; set; }
+    }
+}
