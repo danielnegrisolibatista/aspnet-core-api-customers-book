24c7cd18c42abb7ae7e6b714c8ac326fe30fa7d3
Daniel Negrisoli Batista <danielnegrisoli@gmail.com>
2020-09-14 21:48:15 -0300

    Updated CustomerRepository

---
+++ Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,59 @@
+﻿using aspnet_core_api_data_driven_customers_book.Data.Repositories.Interface;
+using aspnet_core_api_data_driven_customers_book.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet_core_api_data_driven_customers_book.Data.Repositories
+{
+    class CustomerRepository : ICustomerRepository
+    {
+        private readonly DataContext _dataContext;
+        public CustomerRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task SaveAsync(Customer customer)
+        {
+            await _dataContext.Customers
+                .AddAsync(customer)
+                .ConfigureAwait(false);
+        }
+        public Task UpdateAsync(Customer customer)
+        {
+            _dataContext.Customers
+                .Update(customer);
+
+            return Task.CompletedTask;
+        }
+        public Task DeleteAsync(Customer customer)
+        {
+            _dataContext.Customers
+                .Remove(customer);
+
+            return Task.CompletedTask;
+        }
+        public async Task<List<Customer>> Get()
+        {
+            return await _dataContext.Customers
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+        public async Task<Customer> GetById(int customerId)
+        {
+            var customer = await _dataContext.Customers
+                .Where(s => s.Id.Equals(customerId))
+                .SingleOrDefaultAsync()
+                .ConfigureAwait(false);
+
+            if (customer is null)
+            {
+                return null!;
+            }
+
+            return customer;
+        }
+    }
+}
---
+++ Data/Repositories/Interface/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using aspnet_core_api_data_driven_customers_book.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet_core_api_data_driven_customers_book.Data.Repositories.Interface
+{
+    public interface ICustomerRepository
+    {
+        Task SaveAsync(Customer customer);
+        Task UpdateAsync(Customer customer);
+        Task DeleteAsync(Customer customer);
+        Task<List<Customer>> Get();
+        Task<Customer> GetById(int customerId);
+    }
+}
