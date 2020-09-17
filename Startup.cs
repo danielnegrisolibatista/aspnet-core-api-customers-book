044d8551a3530474e787d890ce6f12fcfe884286
Daniel Negrisoli Batista <danielnegrisoli@gmail.com>
2020-09-16 22:02:36 -0300

    Updated Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using aspnet_core_api_data_driven_customers_book.Data;
 using aspnet_core_api_data_driven_customers_book.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace aspnet_core_api_data_driven_customers_book
 {
